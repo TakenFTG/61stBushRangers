c7cda03d31bf78f3da464c2610f4e0097c5130af
TakenFTG <lachlan@wilsonit.net.au>
2024-02-27 22:00:27 +1030

    Added Mod, still need alot of work

---
+++ 61stBushrangersMod/Addons/61st_Core/config.cpp
@@ -0,0 +1,28 @@
+class CfgPatches
+{
+	class 61st_Core
+	{
+		author="61st Bushrangers";
+		name="[61st]: Custom Assets";
+		url="https://discord.gg/3shdsQ5Wrs";
+		units[]={};
+		weapons[]={};
+		magazines[]={};
+		ammo[]={};
+		requiredVersion=1;
+		requiredAddons[]={};
+	};
+};
+class CfgAddons
+{
+	class PreloadAddons
+	{
+		class 61st_Core
+		{
+			list[]=
+			{
+				"61st_Core"
+			};
+		};
+	};
+};
---
+++ 61stBushrangersMod/mod.cpp
@@ -0,0 +1,14 @@
+name="[61st] Assests";
+
+picture="\V_61st_Core\data\UI\61st_Logo_CA.paa";
+
+actionName="Discord";
+action="https://discord.gg/3shdsQ5Wrs";
+
+logoSmall="\V_61st_Core\data\UI\61st_Logo64x_CA.paa";
+logo="\V_61st_Core\data\UI\61st_Logo128x_CA.paa";
+logoOver="\V_61st_Core\data\UI\61st_Logo128x_CA.paa";
+
+tooltipOwned="[61st] Assests";
+
+overview="Fireteam Zulu, is a mod created to accompany Article 2 Studios' Operation: Trebuchet (OPTRE). The original sole purpose for A2D was to create higher quality and more diverse textures for units to use as they battle either the Insurrectionists or even the Covenant.";
