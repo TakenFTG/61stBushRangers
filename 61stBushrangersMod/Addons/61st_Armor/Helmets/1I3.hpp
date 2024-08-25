7d7f84fe53f6ef2e78c9a3a658413109067ad44b
TakenFTG <lachlan@wilsonit.net.au>
2024-08-25 14:09:41 +0930

    Added 1-3 to Helmets, Helmets are complete

---
+++ 61stBushrangersMod/Addons/61st_Armor/Helmets/1I3.hpp
@@ -0,0 +1,110 @@
+    class BR_H_1I3_LDR: BR_H_RSV_PLAIN
+	{
+		displayName="[61st] 1-3 Helmet (LDR)";
+		hiddenSelectionsTextures[]=
+		{
+			"61st_Armor\data\Helmets\1stPlatoon\1-3\BR_H_1I3_LDR",
+			"61st_Armor\data\Helmets\Materials\V_CH252D_DEF_V_CO",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\soft_packs_co.paa"
+		};
+	};
+    class BR_H_1I3_2IC: BR_H_RSV_PLAIN
+	{
+		displayName="[61st] 1-3 Helmet (2IC)";
+		hiddenSelectionsTextures[]=
+		{
+			"61st_Armor\data\Helmets\1stPlatoon\1-3\BR_H_1I3_2IC",
+			"61st_Armor\data\Helmets\Materials\V_CH252D_DEF_V_CO",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\soft_packs_co.paa"
+		};
+	};
+    class BR_H_1I3_RTO: BR_H_RSV_PLAIN
+	{
+		displayName="[61st] 1-3 Helmet (RTO)";
+		hiddenSelectionsTextures[]=
+		{
+			"61st_Armor\data\Helmets\1stPlatoon\1-3\BR_H_1I3_RTO",
+			"61st_Armor\data\Helmets\Materials\V_CH252D_DEF_V_CO",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\soft_packs_co.paa"
+		};
+	};
+    class BR_H_1I3_MED: BR_H_RSV_PLAIN
+	{
+		displayName="[61st] 1-3 Helmet (MED)";
+		hiddenSelectionsTextures[]=
+		{
+			"61st_Armor\data\Helmets\1stPlatoon\1-3\BR_H_1I3_MED",
+			"61st_Armor\data\Helmets\Materials\V_CH252D_DEF_V_CO",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\soft_packs_co.paa"
+		};
+	};
+    class BR_H_1I3_PLAIN: BR_H_RSV_PLAIN
+	{
+		displayName="[61st] 1-3 Helmet (Plain)";
+		hiddenSelectionsTextures[]=
+		{
+			"61st_Armor\data\Helmets\1stPlatoon\1-3\BR_H_1I3_PLAIN",
+			"61st_Armor\data\Helmets\Materials\V_CH252D_DEF_V_CO",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\soft_packs_co.paa"
+		};
+	};
+    class BR_H_1I3_LDR_dp: BR_H_RSV_PLAIN_dp
+	{
+		displayName="[61st] 1-3 Helmet (LDR)";
+		hiddenSelectionsTextures[]=
+		{
+			"61st_Armor\data\Helmets\1stPlatoon\1-3\BR_H_1I3_LDR",
+			"61st_Armor\data\Helmets\Materials\V_CH252D_CLR_V_CA",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\soft_packs_co.paa"
+		};
+	};
+    class BR_H_1I3_2IC_dp: BR_H_RSV_PLAIN_dp
+	{
+		displayName="[61st] 1-3 Helmet (2IC)";
+		hiddenSelectionsTextures[]=
+		{
+			"61st_Armor\data\Helmets\1stPlatoon\1-3\BR_H_1I3_2IC",
+			"61st_Armor\data\Helmets\Materials\V_CH252D_CLR_V_CA",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\soft_packs_co.paa"
+		};
+	};
+    class BR_H_1I3_RTO_dp: BR_H_RSV_PLAIN_dp
+	{
+		displayName="[61st] 1-3 Helmet (RTO)";
+		hiddenSelectionsTextures[]=
+		{
+			"61st_Armor\data\Helmets\1stPlatoon\1-3\BR_H_1I3_RTO",
+			"61st_Armor\data\Helmets\Materials\V_CH252D_CLR_V_CA",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\soft_packs_co.paa"
+		};
+	};
+    class BR_H_1I3_MED_dp: BR_H_RSV_PLAIN_dp
+	{
+		displayName="[61st] 1-3 Helmet (MED)";
+		hiddenSelectionsTextures[]=
+		{
+			"61st_Armor\data\Helmets\1stPlatoon\1-3\BR_H_1I3_MED",
+			"61st_Armor\data\Helmets\Materials\V_CH252D_CLR_V_CA",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\soft_packs_co.paa"
+		};
+	};
+    class BR_H_1I3_PLAIN_dp: BR_H_RSV_PLAIN_dp
+	{
+		displayName="[61st] 1-3 Helmet (Plain)";
+		hiddenSelectionsTextures[]=
+		{
+			"61st_Armor\data\Helmets\1stPlatoon\1-3\BR_H_1I3_PLAIN",
+			"61st_Armor\data\Helmets\Materials\V_CH252D_CLR_V_CA",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\soft_packs_co.paa"
+		};
+	};
