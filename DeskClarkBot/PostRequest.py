a159d6518b1beb760224b7a1a0528be4c3485b87
LachlanRichards375 <lach1an@tpg.com.au>
2024-10-06 13:34:30 +1030

    BOT: Update Post Request to include PATCH and GET for updating commands

--- DeskClarkBot/PostRequest.py
+++ DeskClarkBot/PostRequest.py
@@ -7,31 +7,62 @@ Since discord now only allows commands to be registered using POST requests, use
 import requests
 from Token import *
 
-url = "https://discord.com/api/v10/applications/" + str(APP_ID) + "/guilds/" + str(GUILD_ID) + "/commands"
+def CreateCommand():
+	url = "https://discord.com/api/v10/applications/" + str(APP_ID) + "/guilds/" + str(GUILD_ID) + "/commands"
 
-# This is an example USER command, with a type of 2
-json = {
-    "name": "update_discord_roles",
-    "type": 1,
-	"guild_id": GUILD_ID,
-	"description": "Update Roles within the unit, Will update all roles if no Orbat ID is provided",
-	"options": [
-		{
-			"name": "orbat_id",
-			"description": "Used to update only a single members roles",
-			"type" : 4,
-			"required" : False,
-			"autocomplete" : False
-		}
-	]
-}
+	# This is an example USER command, with a type of 2
+	json = {
+		"name": "update_discord_roles",
+		"type": 1,
+		"guild_id": GUILD_ID,
+		"description": "Update Roles within the unit, Will update all roles if no Orbat ID is provided",
+		"options": [
+			{
+				"name": "orbat_id",
+				"description": "Used to update only a single members roles",
+				"type" : 4,
+				"required" : False,
+				"autocomplete" : False
+			}
+		]
+	}
+	return url, json
+
+def GetAllCommands():
+	url = "https://discord.com/api/v10/applications/" + str(APP_ID) + "/guilds/" + str(GUILD_ID) + "/commands"
+
+	# This is an example USER command, with a type of 2
+	json = {}
+	return url, json
+
+def EditCommand():
+	url = "https://discord.com/api/v10/applications/" + str(APP_ID) + "/guilds/" + str(GUILD_ID) + "/commands/" + str(CMD_ID_UPDATE_DISCORD_ROLES)
+
+	# This is an example USER command, with a type of 2
+	json = {
+		"description": "Update Roles within the unit, Will update all roles if no Orbat IDs are provided",
+		"options": [
+			{
+				"name": "orbat_ids",
+				"description": "Used to update only specific members roles. Separate with commas ','",
+				"type" : 3,
+				"required" : False,
+				"autocomplete" : False
+			}
+		]
+	}
+	return url, json
 
 # For authorization, you can use either your bot token
 headers = {
     "Authorization": "Bot " + TOKEN
 }
 
-r = requests.post(url, headers=headers, json=json)
+command = EditCommand()
+
+# r = requests.get(command[0], headers=headers)
+# r = requests.post(command[0], headers=headers, json=command[1])
+r = requests.patch(command[0], headers=headers, json=command[1])
 
 print(r.content)
-input("Press any key to contine")
+input("Press enter key to contine")
